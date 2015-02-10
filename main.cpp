ea542d0576d3db88f2f8fb236a59585e4689d032
ReenExe <ipourri@gmail.com>
2015-02-10 22:43:41 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: reen
+ *
+ * Created on February 10, 2015, 10:40 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
