f687bcc6dcec48f77dba2f6e142f7c760767c031
ReenExe <ipourri@gmail.com>
2015-02-13 01:34:47 +0200

    clear code

--- main.sh
+++ main.sh
@@ -1,17 +1,21 @@
  #!/bin/bash
 
-getDirectory() {
-    echo $1
+main() {
+    getDirectory() {
+        echo $1
+    }
+
+    mkdir samples
+    cd samples
+
+    for index in 1 2 3 4 5
+    do
+        mkdir $index
+        getDirectory $(pwd)
+    done
+
+    cd ..
+    rm -rf samples
 }
 
-mkdir samples
-cd samples
-
-for index in 1 2 3 4 5
-do
-    mkdir $index
-    getDirectory $(pwd)
-done
-
-cd ..
-rm -rf samples
+main
